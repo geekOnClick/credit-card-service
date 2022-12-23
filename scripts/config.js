3a3d762a4f3a058609bdd1eff111dd70604bbf75
geekOnClick <kozloww1508@yandex.ru>
2022-12-23 07:19:47 +0300

    Finished second module

---
+++ scripts/config.js
@@ -0,0 +1,4 @@
+// Temporary using Window global object until React app will be created
+window.RAPID_API_KEY = 'aec3842a6cmsh2f3487da45acac6p124f36jsn95d5161c12ca';
+window.RAPID_API_HOST = 'currency-exchange.p.rapidapi.com';
+window.NEWS_API_KEY = '26117cff601544f5b4be2768788b8104';
